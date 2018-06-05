40aab63e1f90cd58266f8d439db57df8138d2863
christianLB <k2600x@gmail.com>
2018-06-05 06:56:24 +0100

    bla bla bla

--- src/components/letter.js
+++ src/components/letter.js
@@ -1,4 +1,4 @@
-import {TweenMax, TimelineMax, Expo} from 'gsap/all';
+import {TweenMax, TimelineMax, Expo, Circ} from 'gsap/all';
 
 import React, {Component} from 'react';
 import './letter.less';
@@ -6,82 +6,76 @@ import './letter.less';
 class Letter extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            delay: this.props.delay,
-            speed: this.props.speed,
-        };
         this.container = React.createRef();
-        this.explode = new TimelineMax({paused: true});
-        this.caer = new TimelineMax({paused: true});
+        this.explode = {};
+        this.caer = {};
+        this.fontSize = this.getRandom(20, 90);
+        this.explode = new TimelineMax({callbackScope: this});
+        this.caer = new TimelineMax({callbackScope: this});
+        this.speed = 5;
     }
     componentDidMount() {
-        setTimeout(() => {
-            this.init();
-        }, 1);
+        this.caida();
     }
     getRandom(min, max) {
         return Math.floor(Math.random() * (max - min) ) + min;
     }
-    init() {
-        let container = this.container.current;
-        let letter = this.container.current;
-        let exp = letter.querySelector('.exp');
-        let exp2 = letter.querySelector('.exp2');
-        let explode = this.explode;
-
-        let onStart = () => {
-            let exp = this.container.current.querySelector('.exp');
-            let exp2 = this.container.current.querySelector('.exp2');
-            let fontSize = this.getRandom(25, 80);
-            TweenMax.to(container, 0,
-                {bottom: document.body.offsetHeight,
-                 left: this.getRandom(0, this.props.maxX),
-                 fontSize: fontSize,
-                 opacity: 1,
-                }
-            );
-            exp.style.width = `${fontSize}px`;
-            exp.style.height = `${fontSize}px`;
-            exp2.style.width = `${fontSize}px`;
-            exp2.style.height = `${fontSize}px`;
-            this.caer.timeScale(1.1);
-        };
-        // ///////////////////////////////////////////
-        this.caer.to(container, 10,
-                    {bottom: 0,
-                     delay: this.getRandom(1, 10),
-                    },
-        );
-        this.caer.eventCallback('onStart', onStart);
-        this.caer.eventCallback('onComplete', () => {
-            this.props.onComplete(this);
-            this.caer.restart();
+    caida() {
+        let c = TweenMax.to(this.container.current, 2, {bottom: 0});
+        this.caer.to(this.container.current, 0, {opacity: 0});
+        this.caer.to(this.container.current, 0, {
+          bottom: document.body.offsetHeight,
+          left: this.getRandom(0, this.props.maxX),
+          fontSize: this.getRandom(20, 90),
+          opacity: 1,
+          delay: this.getRandom(1, 20),
+        }, 0.1);
+        this.caer.add(c);
+        /* this.caer.to(this.container.current, this.speed,
+          {bottom: -this.fontSize, ease: Circ.easeIn},
+        ); */
+        c.eventCallback('onComplete', () => {
+          this.caer.pause();
+          this.props.onComplete(this);
+          this.speed = 1;
+          this.fontSize = this.getRandom(20, 90);
+          // this.caer.restart();
+          this.caida();
         });
-        // ///////////////////////////////////////////
-        explode.eventCallback('onComplete', () => {
-            this.explode.restart().pause();
-            this.caer.restart();
-        });
-        explode.to([exp, exp2], 0.1, {opacity: 1});
-        explode.to(exp2, 0.5, {
-          scale: 4,
-          border: '5px solid red',
-          opacity: 0,
-          ease: Expo.easeOut,
-        }, 0.2);
-        explode.to(exp, 1.2, {
-            scale: 1.5,
-            ease: Expo.easeOut}, 0);
-        explode.to(exp, 0.5, {backgroundColor: 'yellow'}, 0.3);
-        explode.to(exp, 0.5, {backgroundColor: 'red', opacity: 0}, 0.6);
-        explode.to(letter, 0.2, {opacity: 0}, 0.6);
-        explode.to(letter, 1.2, {
-            scale: 2, color: 'yellow', ease: Expo.easeOut}, 0.1);
         this.caer.play();
     }
     destroy() {
-        this.caer.pause();
-        this.explode.play();
+      let letter = this.container.current;
+      let exp = letter.querySelector('.exp');
+      let exp2 = letter.querySelector('.exp2');
+      let explode = this.explode;
+
+      explode.to([exp, exp2], 0, {
+        width: `${this.fontSize}px`,
+        height: `${this.fontSize}px`,
+        opacity: 1,
+      }, 0.1);
+      explode.set([exp, exp2], {scale: 1});
+      explode.eventCallback('onComplete', () => {
+        this.fontSize = this.getRandom(20, 90);
+        this.explode.restart().pause();
+        this.caer.restart().pause();
+        this.caida();
+      });
+      // explode2 /////////////////////////
+      explode.to(exp2, 0.5, {
+        scale: 4,
+        border: '5px solid red',
+        opacity: 0,
+        ease: Expo.easeOut,
+      }, 0.2);
+      explode.to(exp, 1.2, {scale: 1.5, ease: Expo.easeOut}, 0);
+      explode.to(exp, 0.5, {backgroundColor: 'yellow'}, 0.3);
+      explode.to(exp, 0.5, {backgroundColor: 'red', opacity: 0}, 0.6);
+      explode.to(letter, 0.2, {opacity: 0}, 0.6);
+      explode.to(letter, 1.2, {scale: 2, color: 'yellow', ease: Expo.easeOut}
+      , 0.1);
+      this.explode.play();
     }
     translate(x, y) {
         x=isNaN(x)?0:x;
@@ -97,7 +91,6 @@ class Letter extends Component {
     render() {
         return (
             <div
-                style={this.state.style}
                 className={this.props.className}
                 ref={this.container}
             >
