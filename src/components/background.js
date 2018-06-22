dcc2a2547deddca6ad34ceb5c671c53dd5fdb876
christianLB <k2600x@gmail.com>
2018-06-22 22:54:17 +0100

    agregué una línea

--- src/components/background.js
+++ src/components/background.js
@@ -7,6 +7,7 @@ class Background extends Component {
         this.background = React.createRef();
         this.stars = React.createRef();
         this.sun = React.createRef();
+        this.sun = React.createRef();
         this.state = {
           stars1: [],
           stars2: [],
